f3d7a0de02c4733b8dd483af73f3ab77aab4af2c
ziaulqamar72 <116137565+ziaulqamar72@users.noreply.github.com>
2023-04-24 17:45:24 +0500

    Update Home.js

--- Home.js
+++ Home.js
@@ -1,28 +1,58 @@
-﻿Office.onReady(function () {
-    //var doc = Office.context.document;
-    //doc.addHandlerAsync(Office.EventType.DocumentSelectionChanged, function (eventArgs) {
-    //    // do something when the selection changes
-    //});
-    // Add event listener on keypress
-    document.addEventListener('keypress', (event) => {
-        var name = event.key;
-        var code = event.code;
-        console.log(event.ctrlKey);
-        // Alert the key name and key code on keydown
-        console.log(`Key pressed ${name} \r\n Key code value: ${code}`);
+var app = angular.module('myApp', []);
+app.controller('myCtrl', function ($scope) {
 
 
-        if (code === 'Backquote') {
-            Office.addin.hide();
+
+    Office.onReady(function () {
+        var currentActiveSheet;
+        var listOfDeactiveSheets = [];
+
+        Excel.run(function (context) {
+            const sheet = context.workbook.worksheets;
+            sheet.onDeactivated.add(deactiveSheets);
+            return context.sync().then(function () {
+
+                //   console.log(currentActiveSheet);
+            });
+        });
+        deactiveSheets();
+
+        function deactiveSheets(event) {
+            listOfDeactiveSheets = [];
+            $scope.currentDeactiveSheets=[];
+         //   console.log(event.worksheetId);
+            //   console.log();
+            Excel.run(function (context) {
+                let activeSheet = context.workbook.worksheets.getActiveWorksheet();
+                let sheets = context.workbook.worksheets;
+                sheets.load("items/name");
+                return context.sync().then(function () {
+                    currentActiveSheet = activeSheet.load("name");
+                    
+                
+                    let totelSheets = sheets.items;
+                    console.log(totelSheets);
+                    for (let i = 0; i < totelSheets.length; i++) {
+                        if (totelSheets[i].id != currentActiveSheet.id) {
+                            listOfDeactiveSheets.push(totelSheets[i].name)
+                        } else {
+                            $scope.CurrentActiveSheetName = totelSheets[i].name;
+                        }
+                        if (!$scope.$$phase) {
+                            $scope.$apply();
+                        }
+                    }
+                    console.log(listOfDeactiveSheets);
+                    $scope.currentDeactiveSheets = listOfDeactiveSheets;
+                    if (!$scope.$$phase) {
+                        $scope.$apply();
+                    }
+                });
+            });
         };
 
 
+    })
 
+});
 
-
-
-
-
-    }, false);
-
-})
