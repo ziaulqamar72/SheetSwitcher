12111007c37b10c67b65f04a96903f79237176ef
ziaulqamar72 <116137565+ziaulqamar72@users.noreply.github.com>
2023-04-24 12:53:38 +0500

    Test RunTime Code

---
+++ FunctionFile.js
@@ -0,0 +1,91 @@
+﻿// The initialize function must be run each time a new page is loaded.
+(function () {
+    Office.initialize = function (reason) {
+        // If you need to initialize something you can do so here.
+    };
+})();
+
+function Add(first, second) {
+
+
+    var values = [
+        [Math.floor(Math.random() * 1000), Math.floor(Math.random() * 1000), Math.floor(Math.random() * 1000)],
+        [Math.floor(Math.random() * 1000), Math.floor(Math.random() * 1000), Math.floor(Math.random() * 1000)],
+        [Math.floor(Math.random() * 1000), Math.floor(Math.random() * 1000), Math.floor(Math.random() * 1000)]
+    ];
+
+    // Run a batch operation against the Excel object model
+    Excel.run(function (ctx) {
+        // Create a proxy object for the active sheet
+        var sheet = ctx.workbook.worksheets.getActiveWorksheet();
+        // Queue a command to write the sample data to the worksheet
+        sheet.getRange("B3:D5").values = values;
+
+        // Run the queued-up commands, and return a promise to indicate task completion
+        return ctx.sync();
+    })
+   
+  return first + second;
+}
+CustomFunctions.associate("ADD", Add);
+
+
+function ShowTaskPane(inputValue) {
+    let action = inputValue.toUpperCase();
+    if (action == "SHOW") {
+        Office.addin.showAsTaskpane();
+    }
+    else if (action == "HIDE") {
+        Office.addin.hide();
+    } else {
+        inputValue = "";
+    }
+    var taskpane = action;
+    return taskpane;
+}
+CustomFunctions.associate("ShowTaskPane", ShowTaskPane);
+
+
+
+
+Office.actions.associate('SHOWTASKPANE', function () {
+    return Office.addin.showAsTaskpane()
+        .then(function () {
+            return;
+        })
+        .catch(function (error) {
+            return error.code;
+        });
+});
+
+
+Office.actions.associate('HIDETASKPANE', function () {
+    return Office.addin.hide()
+        .then(function () {
+            return;
+        })
+        .catch(function (error) {
+            return error.code;
+        });
+});
+
+
+
+
+
+
+//Office.initialize = function (reason) {
+//    // The Office runtime is now initialized
+//    // Set up event listeners here
+
+//    // Set up event listener for keydown event on the document
+//    if (Office.context.document) {
+//        Office.context.document.addHandlerAsync("documentSelectionChanged", onKeyDown);
+//    }
+//}
+
+//function onKeyDown(eventArgs) {
+//    // Handle the keydown event here
+//    var key = eventArgs.originalEvent.code;
+//    console.log("Key pressed: " + key);
+//}
---
+++ Home.js
@@ -0,0 +1,28 @@
+﻿Office.onReady(function () {
+    //var doc = Office.context.document;
+    //doc.addHandlerAsync(Office.EventType.DocumentSelectionChanged, function (eventArgs) {
+    //    // do something when the selection changes
+    //});
+    // Add event listener on keypress
+    document.addEventListener('keypress', (event) => {
+        var name = event.key;
+        var code = event.code;
+        console.log(event.ctrlKey);
+        // Alert the key name and key code on keydown
+        console.log(`Key pressed ${name} \r\n Key code value: ${code}`);
+
+
+        if (code === 'Backquote') {
+            Office.addin.hide();
+        };
+
+
+
+
+
+
+
+
+    }, false);
+
+})
